a6214d3401ab2f9f48b2d457c85ce001412d83e7
noguchi-takanori <ssj7772g@i.softbank.jp>
2021-09-25 14:28:15 +0900

    出品機能実装して

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,6 +5,28 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
+    @category = Category.all
+    @status = Status.all
+    @payment = Payment.all
+    @shipment = Shipment.all
+    @scheduled = Scheduled.all
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.valid? 
+      @item.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:item_name, :price, :description_item, :detail_category_id, :detail_status_id, 
+      :payment_method_id, :shipment_source_id, :scheduled_day_id, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -11,4 +11,29 @@ class Item < ApplicationRecord
   validates :payment_method_id,  presence: true
   validates :shipment_source_id, presence: true
   validates :scheduled_day_id,   presence: true
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :detail_category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :status
+
+  validates :detail_status_id, numericality: { other_than: 1 , message: "detail can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :payment
+
+  validates :payment_method_id, numericality: { other_than: 1 , message: "payment can't be blank"} 
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :shipment
+
+  validates :shipment_source_id, numericality: { other_than: 1 , message: "shipment can't be blank"}
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :scheduled
+
+  validates :scheduled_day_id, numericality: { other_than: 1 , message: "scheduled can't be blank"}
 end
