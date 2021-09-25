8be6bff249e36334d4d346a889ec1005eff21d96
noguchi-takanori <ssj7772g@i.softbank.jp>
2021-09-25 18:42:03 +0900

    修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,18 @@
 FactoryBot.define do
   factory :item do
-    price              {"3000"}
+    price              {3000}
     item_name          {"商品テスト"}
     description_item   {"商品テスト"}
-    detail_category_id {"11"}
-    detail_status_id   {"7"}
-    payment_method_id  {"3"}
-    shipment_source_id {"48"}
-    scheduled_day_id   {"4"}
-    user
+    detail_category_id {11}
+    detail_status_id   {7}
+    payment_method_id  {3}
+    shipment_source_id {48}
+    scheduled_day_id   {4}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -4,7 +4,6 @@ RSpec.describe Item, type: :model do
 
   before do
     @item = FactoryBot.build(:item)
-    @item.image = fixture_file_upload("/files/test_image.JPG")
   end
 
   describe '商品出品' do
@@ -68,6 +67,16 @@ RSpec.describe Item, type: :model do
         @item.price = "10000000"
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+      it '商品画像が空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
       end
     end
   end
