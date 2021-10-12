d8c04f034f201c585f7abfb16e97b3d9a612d2fb
noguchi-takanori <ssj7772g@i.softbank.jp>
2021-10-12 20:20:34 +0900

    修正

--- db/migrate/20210918064152_create_items.rb
+++ db/migrate/20210918064152_create_items.rb
@@ -5,11 +5,11 @@ class CreateItems < ActiveRecord::Migration[6.0]
       t.integer    :price,              null: false
       t.string     :item_name,          null: false
       t.text       :description_item,   null: false
-      t.integer    :category_id, null: false
-      t.integer    :status_id,   null: false
-      t.integer    :payment_id,  null: false
-      t.integer    :shipment_id, null: false
-      t.integer    :scheduled_id,   null: false
+      t.integer    :category_id,        null: false
+      t.integer    :status_id,          null: false
+      t.integer    :payment_id,         null: false
+      t.integer    :shipment_id,        null: false
+      t.integer    :scheduled_id,       null: false
       t.references :user,               null: false, foreign_key: true
       t.timestamps
     end
